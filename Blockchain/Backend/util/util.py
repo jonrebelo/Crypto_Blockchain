713ad4e4c94adb2cb8733c4306e001fd088b91a6
jonrebelo <jonathanrebelo@protonmail.com>
2024-05-15 12:00:26 -0400

    added hash160 function for keys and address generation

--- Blockchain/Backend/util/util.py
+++ Blockchain/Backend/util/util.py
@@ -1,5 +1,10 @@
 import hashlib
+from Crypto.Hash import RIPEMD160
+from hashlib import sha256
 
 def hash256(s):
     """Two passes of SHA 256"""
     return hashlib.sha256(hashlib.sha256(s).digest()).digest()
+
+def hash160(s):
+    return RIPEMD160.new(sha256(s).digest()).digest()
