d4e0b335f8c4357ccbc40d7dc51e1026e17d9013
jonrebelo <jonathanrebelo@protonmail.com>
2024-05-16 17:42:11 -0400

    Corrected typo in comments

--- Blockchain/client/account.py
+++ Blockchain/client/account.py
@@ -20,7 +20,7 @@ class account:
         x_point = uncompressed_public_key.x
         y_point = uncompressed_public_key.y
 
-        #define the prefix of the public key. Written in bytes. big refers to "big indian" which writes the code from left to right. little would be the opposite. Then create public key
+        #define the prefix of the public key. Written in bytes. big refers to "big endian" which writes the code from left to right. little would be the opposite. Then create public key
         if y_point.num % 2 == 0:
             compressed_key = b'\x02' + x_point.num.to_bytes(32, 'big')
         else:
