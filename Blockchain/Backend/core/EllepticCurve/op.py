cbc05303a1230b2122216ea29ade42a830a5a477
jonrebelo <jonathanrebelo@protonmail.com>
2024-05-18 16:37:25 -0400

    Added documentation on the op stack functions

--- Blockchain/Backend/core/EllepticCurve/op.py
+++ Blockchain/Backend/core/EllepticCurve/op.py
@@ -1,22 +1,24 @@
-""" 
-Copyright (c) 2021 Codiesalert.com
-These scripts shouldn't be used for commercial purpose without Codies Alert Permission
-Any violations may lead to legal action
-"""
 from Blockchain.Backend.util.util import hash160
 from Blockchain.Backend.core.EllepticCurve.EllepticCurve import Sha256Point, Signature
 
 
 def op_dup(stack):
-
+    """
+    Duplicates the top item on the stack.
+    If the stack is empty, returns False.
+    """
     if len(stack) < 1:
         return False
     stack.append(stack[-1])
-
     return True
 
 
 def op_hash160(stack):
+    """
+    Removes the top item from the stack, hashes it with SHA-256, then RIPEMD-160,
+    and pushes the result back onto the stack.
+    If the stack is empty, returns False.
+    """
     if len(stack) < 1:
         return False
     element = stack.pop()
@@ -26,6 +28,10 @@ def op_hash160(stack):
 
 
 def op_equal(stack):
+    """
+    Removes the top two items from the stack, compares them and pushes the result (1 if equal, 0 otherwise) onto the stack.
+    If there are fewer than two items on the stack, returns False.
+    """
     if len(stack) < 2:
         return False
 
@@ -41,6 +47,10 @@ def op_equal(stack):
 
 
 def op_verify(stack):
+    """
+    Removes the top item from the stack and checks if it is non-zero.
+    If it is zero or if the stack is empty, returns False.
+    """
     if len(stack) < 1:
         False
     element = stack.pop()
@@ -52,10 +62,18 @@ def op_verify(stack):
 
 
 def op_equalverify(stack):
+    """
+    Performs the op_equal operation and then op_verify on the result.
+    """
     return op_equal(stack) and op_verify(stack)
 
 
 def op_checksig(stack, z):
+    """
+    Checks the signature of the top item of the stack against the second item on the stack (which should be a public key),
+    using the provided hash 'z'. If the signature is valid, pushes 1 onto the stack, otherwise pushes 0.
+    If there are fewer than two items on the stack, returns False.
+    """
     if len(stack) < 1:
         return False
 
