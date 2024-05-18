db785087ededec06b367041f19ea35e096aa0021
jonrebelo <jonathanrebelo@protonmail.com>
2024-05-18 17:07:56 -0400

    Added function to use functions from op.py to verify transactions

--- Blockchain/Backend/core/script.py
+++ Blockchain/Backend/core/script.py
@@ -1,11 +1,17 @@
 from Blockchain.Backend.util.util import int_to_little_endian, encode_varint
-
+from Blockchain.Backend.core.EllepticCurve.op import OP_CODE_FUNCTION
 class Script:
     def __init__(self, cmds = None):
         if cmds is None:
             self.cmds = []
         else:
             self.cmds = cmds
+
+    def __add__(self, other):
+        #override addition operation to add commands when script is added
+        return Script(self.cmds + other.cmds)
+
+    
     def serialize(self):
         #initialize what's being sent back
         result = b''
@@ -40,6 +46,50 @@ class Script:
 
         #encode_varint the total length
         return encode_varint(total) + result
+
+    def evaluate(self, z):
+
+        """This function evaluates a list of commands (self.cmds) using a stack-based approach. 
+    It iterates over the commands, and for each command, it performs an operation based on its type.
+    Parameters:
+    z: A hash used in the op_checksig operation.
+    Returns:
+    None. However, it prints an error message and returns False if an operation fails."""
+
+        # Copy the list of commands
+        cmds = self.cmds[:]
+
+        # Initialize an empty stack
+        stack = []
+
+        # Iterate over the commands while there are commands left
+        while len(cmds) > 0:
+
+            # Pop the first command from the list
+            cmd = cmds.pop(0)
+
+            # If the command is an integer, it's an operation code
+            if type(cmd) == int:
+
+                # Get the corresponding operation function from the OP_CODE_FUNCTION dictionary
+                operation = OP_CODE_FUNCTION[cmd]
+
+                # If the operation code is 172 (op_checksig), perform the operation with the stack and z as arguments
+                if cmd == 172:
+                    if not operation(stack, z):
+                        print(f"error in Signature Verification")
+                        return False
+
+                # For other operation codes, perform the operation with the stack as the argument
+                if not operation(stack):
+                    print(f"error in operation")
+                    return False
+
+            # If the command is not an integer, it's a value to be pushed onto the stack
+            else:
+                stack.append(cmd)
+        
+        return True
 
     @classmethod
     def p2pkh_script(cls, h160):
