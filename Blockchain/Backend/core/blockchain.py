7951bab6512f1291ab4f4d465043b10623edb7bd
jonrebelo <jonathanrebelo@protonmail.com>
2024-05-21 14:06:50 -0400

    Added functionality to add transactions to block. Added print functions to confirm transactions appear in MemPool

--- Blockchain/Backend/core/blockchain.py
+++ Blockchain/Backend/core/blockchain.py
@@ -19,6 +19,7 @@ class Blockchain:
     def __init__(self, utxos, MemPool):
         self.utxos = utxos
         self.MemPool = MemPool
+        print(f"Blockchain initialized with UTXOS: {utxos} and MemPool: {MemPool}") 
 
     def write_on_disk (self, block):
         blockchaindb = BlockchainDB()
@@ -27,8 +28,7 @@ class Blockchain:
     def fetch_last_block(self):
         blockchaindb = BlockchainDB()
         return blockchaindb.lastBlock()
-
-
+    
 #create the first block
     def GenesisBlock(self):
         block_height = 0
@@ -39,23 +39,49 @@ class Blockchain:
         """Keep track of unspent transactions in cache"""
         self.utxos[Transaction.TxId] = Transaction
 
+    def read_transaction_from_mempool(self):
+        """Read transactions from the mempool and store them in a list to be added to the block"""
+        self.TxIds = []
+        self.add_transactions_in_block = []
+        
+
+        for tx in self.MemPool:
+            print(f"Reading transaction {tx} from mempool")
+            self.TxIds.append(tx)
+            self.add_transactions_in_block.append(self.MemPool[tx])
+
+    def convert_to_json(self):
+        self.TxJson = []
+
+        for tx in self.add_transactions_in_block:
+            self.TxJson.append(tx.to_dict())
+
 #create subsequent blocks
     def addBlock(self, block_height, prev_block_hash):
+        self.read_transaction_from_mempool()
         timestamp = int(time.time())
         coinbase_instance = Coinbase_Tx(block_height)
         coinbase_tx = coinbase_instance.Coinbase_Transaction()
+
+        self.TxIds.insert(0, coinbase_tx.TxId)
+        self.add_transactions_in_block.insert(0, coinbase_tx)
+
         merkleRoot = coinbase_tx.TxId
         bits = 'ffff001f'
         blockheader = BlockHeader(version, prev_block_hash, merkleRoot, timestamp, bits)
         blockheader.mine()
         self.store_utxos_in_cache(coinbase_tx)
+        self.convert_to_json()
         print(f"Block {block_height} mined successfully with Nonce value {blockheader.nonce}")
-        self.write_on_disk([Block(block_height, 1, blockheader.__dict__, 1, coinbase_tx.to_dict()).__dict__])
+        self.write_on_disk([Block(block_height, 1, blockheader.__dict__, 1, self.TxJson).__dict__])
+        print(f"Block {block_height} written on disk")
+        self.write_on_disk([Block(block_height, 1, blockheader.__dict__, 1, self.TxJson).__dict__])
         
 #Add new blocks to the chain
     def main(self):
         lastBlock = self.fetch_last_block()
         if lastBlock is None:
+            print("Creating genesis block")
             self.GenesisBlock()
         while True:
             #Locate last block
@@ -65,16 +91,17 @@ class Blockchain:
             #Retrieve the last block hash
             prev_block_hash = lastBlock["BlockHeader"]["blockHash"]
             #pass to addBlock method
+            print(f"Adding block {block_height} to the blockchain")
             self.addBlock(block_height, prev_block_hash)
 
 
 
 if __name__ == "__main__":
-    
-    #Create a dictionary to store the UTXOs
     with Manager() as manager:
         utxos = manager.dict()
         MemPool = manager.dict()
+
+        print(f"Manager started with UTXOS: {utxos} and MemPool: {MemPool}")  # Print the initial UTXOS and MemPool
 
         #Start the frontend on a separate process
         webapp = Process(target=main, args=(utxos, MemPool))
@@ -82,3 +109,4 @@ if __name__ == "__main__":
         
         blockchain = Blockchain(utxos, MemPool)
         blockchain.main()
+    
